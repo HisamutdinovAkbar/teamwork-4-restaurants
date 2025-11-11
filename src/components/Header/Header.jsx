1d02ac0a5c78cd931901dd285fd73871d736d913
hisamutdinovakbar48-lgtm <hisamutdinovakbar48@gmail.com>
2025-11-11 14:01:14 +0500

    akbar header done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,35 @@
 import React from 'react'
-import "./Header.css";
-
+import "./Header.css"
+import logo from "/public/logo.png";
+import logo1 from "/public/logo1.png";
+import logo2 from "/public/logo2.png";
+import logo3 from "/public/logo3.png";
 function Header() {
   return (
-    <header>
-        <div className='container'>
-<p>header</p>
-        </div>
-    </header>
+  <header>
+<nav>
+    <div className='container'>
+<div className="parent">
+    <div className="left-nav">
+<img src={logo} alt="" />
+  </div>
+  <div className="right-nav">
+<div className="fr-div">
+  <img  src={logo1} alt="" /> 
+<p>Log in</p>
+</div>
+<div className="sc-div">
+<img src={logo2} alt="" />
+<p>0</p>
+<img className='img1' src={logo3} alt="" /> 
+</div>
+
+
+  </div>
+</div>
+    </div>
+</nav>
+  </header>
   )
 }
 
