f85c5431d0613b834593b3ab50f48700e8c9c0bc
tohirbek6768-png <tohirbek6768@gmail.com>
2025-11-11 14:12:26 +0500

    first push

--- src/components/Main/Section4/Section4.jsx
+++ src/components/Main/Section4/Section4.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
-import "./Section4.css";
+import "./Section4.css"
+ import logo from "/public/images/logo.png";
+  import logo_1 from "/public/images/logo_1.png";
+   import logo_2 from "/public/images/logo_2.png";
+    import logo_3 from "/public/images/logo_3.png";
+     import insta from "/public/images/insta.png";
 function Section4() {
   return (
     <section>
-        <div className='container'>
-<p>Section4</p>
+        <div className='section1'>
+<img src={insta} alt="" />
         </div>
+<div className='section2'>
+  <img src={logo} alt="" />
+    <img src={logo_1} alt="" />
+      <img src={logo_2} alt="" />
+        <img src={logo_3} alt="" />
+</div>
     </section>
   )
 }
